fa5a575b7419bb885163def410b237b85eb16488
Christos Christou <christosx132@gmail.com>
2023-03-05 04:12:47 +0000

    added maping function for the card component and removed repeting code.

--- src/components/App.js
+++ src/components/App.js
@@ -2,34 +2,24 @@ import React from "react";
 import Card from "./Card";
 import contacts from "../contacts";
 
+//a function for maping the data of the component
+function createCard(contact) {
+  return (
+    <Card
+      key={contact.id}
+      name={contact.name}
+      img={contact.imgURL}
+      tel={contact.phone}
+      email={contact.email}
+    />
+  );
+}
+
 function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
-      {/* get the card details from the contacts.js file */}
-      <Card
-        name={contacts[0].name}
-        img={contacts[0].imgURL}
-        alt="Beyonce avatar"
-        tel={contacts[0].phone}
-        email={contacts[0].email}
-      />
-
-      <Card
-        name={contacts[1].name}
-        img={contacts[1].imgURL}
-        alt="Jack Bauer avatar"
-        tel={contacts[1].phone}
-        email={contacts[1].email}
-      />
-
-      <Card
-        name={contacts[2].name}
-        img={contacts[2].imgURL}
-        alt="Chuck Norris avatar"
-        tel={contacts[2].phone}
-        email={contacts[2].email}
-      />
+      {contacts.map(createCard)}
     </div>
   );
 }
